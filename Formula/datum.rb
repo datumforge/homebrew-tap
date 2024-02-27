661e6f715f2a3c1db7ebd593e94137e2e45ca199
bender-datum <bender@datum.net>
2024-02-26 18:35:11 -0700

    Brew formula update for datum version v0.3.3

--- Formula/datum.rb
+++ Formula/datum.rb
@@ -5,20 +5,20 @@
 class Datum < Formula
   desc "datum is the client CLI for interacting with Datum Server"
   homepage "https://github.com/datumforge/datum"
-  version "0.3.2"
+  version "0.3.3"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/datumforge/datum/releases/download/v0.3.2/datum_0.3.2_darwin_arm64.tar.gz"
-      sha256 "e0cffe5333b1809a10c3e99435a2287a35352e2a697a675bbe40149307dd522c"
+    if Hardware::CPU.intel?
+      url "https://github.com/datumforge/datum/releases/download/v0.3.3/datum_0.3.3_darwin_amd64.tar.gz"
+      sha256 "6ee7fd465cea2b3ac9bb6f19d05255311e3ba69f91d07805e973ea57ec6048be"
 
       def install
         bin.install "datum"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/datumforge/datum/releases/download/v0.3.2/datum_0.3.2_darwin_amd64.tar.gz"
-      sha256 "fe9ed0fa5450e74876b10456ab38653bf68f62dad63e78f81b0749878c6ebfe7"
+    if Hardware::CPU.arm?
+      url "https://github.com/datumforge/datum/releases/download/v0.3.3/datum_0.3.3_darwin_arm64.tar.gz"
+      sha256 "f70b687e57f44dee6e01c5eb5ba18d13e60d0c0659ec12f37f1b2ff0f8314c2c"
 
       def install
         bin.install "datum"
@@ -28,16 +28,16 @@ class Datum < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/datumforge/datum/releases/download/v0.3.2/datum_0.3.2_linux_arm64.tar.gz"
-      sha256 "718294c4603a3907167529296783c8121d4a88f4ccf2e9f8034423d7248ecbe9"
+      url "https://github.com/datumforge/datum/releases/download/v0.3.3/datum_0.3.3_linux_arm64.tar.gz"
+      sha256 "33db78eb55ddfb62f23b79455e2d892a5981ba0af3db34077cf900dc5caae2f8"
 
       def install
         bin.install "datum"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/datumforge/datum/releases/download/v0.3.2/datum_0.3.2_linux_amd64.tar.gz"
-      sha256 "8c66413765349f1b3e9e2e2bcaeda392b0e264179ee5c463198e1dea1e79e6de"
+      url "https://github.com/datumforge/datum/releases/download/v0.3.3/datum_0.3.3_linux_amd64.tar.gz"
+      sha256 "d97f42d89dcc0bad64537958e7df3421c03ea3c351c6744e65210d7b1a4e5e0c"
 
       def install
         bin.install "datum"
