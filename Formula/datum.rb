e0b08bfe680463c3e8f1475641bc591e045a85b8
bender-datum <bender@datum.net>
2024-05-17 12:08:14 -0400

    Brew formula update for datum version v0.5.2

--- Formula/datum.rb
+++ Formula/datum.rb
@@ -5,20 +5,20 @@
 class Datum < Formula
   desc "datum is the client CLI for interacting with Datum Server"
   homepage "https://github.com/datumforge/datum"
-  version "0.5.1"
+  version "0.5.2"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/datumforge/datum/releases/download/v0.5.1/datum_0.5.1_darwin_amd64.tar.gz"
-      sha256 "7b15b92d140563d5b988b239c066b75c730a11e9c185cea7e44ee671274bdea2"
+    on_intel do
+      url "https://github.com/datumforge/datum/releases/download/v0.5.2/datum_0.5.2_darwin_amd64.tar.gz"
+      sha256 "db3e13aa7e9e31de271d2ec835a29ec8425a4f2ca54553921405ab2cbe7976c5"
 
       def install
         bin.install "datum"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://github.com/datumforge/datum/releases/download/v0.5.1/datum_0.5.1_darwin_arm64.tar.gz"
-      sha256 "3f7251a209cdaeeb1d384b4cc89cac55360c0404f58bcf93d2ee0d34d5f5df43"
+    on_arm do
+      url "https://github.com/datumforge/datum/releases/download/v0.5.2/datum_0.5.2_darwin_arm64.tar.gz"
+      sha256 "3bdc1d1129ce2ebfdc4c488ff3f4e3bc4af755603b5e1d63bc5da240639e58c9"
 
       def install
         bin.install "datum"
@@ -27,20 +27,24 @@ class Datum < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/datumforge/datum/releases/download/v0.5.1/datum_0.5.1_linux_amd64.tar.gz"
-      sha256 "f684c9a30ad0136fc0d35d6b0d41d3d428e2eef575785399fa780bc0bf307e77"
+    on_intel do
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/datumforge/datum/releases/download/v0.5.2/datum_0.5.2_linux_amd64.tar.gz"
+        sha256 "12e56fccaab5e810fac35722c358c12889e07f6bc2ca3ad36195368dffe1dedf"
 
-      def install
-        bin.install "datum"
+        def install
+          bin.install "datum"
+        end
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/datumforge/datum/releases/download/v0.5.1/datum_0.5.1_linux_arm64.tar.gz"
-      sha256 "4ad8ca53ce26bb6e2367cef3a678e267bf5ef08bc23034076799590c1345a660"
+    on_arm do
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/datumforge/datum/releases/download/v0.5.2/datum_0.5.2_linux_arm64.tar.gz"
+        sha256 "06c78593e98a71eb8fc6028b0ba08e3f5c32a23edcba66545af95a47a717de5f"
 
-      def install
-        bin.install "datum"
+        def install
+          bin.install "datum"
+        end
       end
     end
   end
